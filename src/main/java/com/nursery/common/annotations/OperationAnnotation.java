9715b8b7bf614d7f7b58d6d1b5bebfbf35398f5e
zhangtianhao <z15938361207@163.com>
2020-12-15 21:30:45 +0800

    操作日志存库

--- src/main/java/com/nursery/common/annotations/OperationAnnotation.java
+++ src/main/java/com/nursery/common/annotations/OperationAnnotation.java
@@ -1,6 +1,8 @@
 package com.nursery.common.annotations;
 
 
+import org.springframework.core.annotation.Order;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -8,6 +10,7 @@ import java.lang.annotation.Target;
 
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Order(1)
 public @interface OperationAnnotation {
 
     /**
